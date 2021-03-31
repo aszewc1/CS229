3bd488a0c9d4f0c6670034d1b88cf178dc4e336f
aszewc1 <alexandra.szewc@gmail.com>
2021-03-30 22:14:35 -0400

    implemented most of driver

--- csf_assign04/imgproc.c
+++ csf_assign04/imgproc.c
@@ -6,14 +6,17 @@
 int main(int argc, char **argv) {
 
   // Load Plugins
+  struct Plugin *plugins = loadPlugins();
+  int numPlugins = sizeof(plugins) / sizeof(struct Plugin);
   
   if (argc <= 1) {
     printUsg();
   } else if (argc == 2 && strcmp("list", argv[1]) == 0) {
     // Print plugin list
+    printPlugins(plugins, numPlugins);
   } else if (argc >= 3 && strcmp("exec", argv[1]) == 0) {
     // Load plugin from argv[2] and parse arguments
-    if (args == NULL) { printErr("missing or invalid command line arguments"); }
+    //if (args == NULL) { printErr("missing or invalid command line arguments"); }
   } else {
     printErr("unknown command name");
   }
@@ -34,9 +37,65 @@ void printErr(const char *msg) {
 }
 
 char *getPluginDir() {
-  char *dir = getnv("PLUGIN_DIR");
+  char *dir = getenv("PLUGIN_DIR");
   if (dir != NULL) {
     return dir;
   }
-  return strcat(getnv("PATH"), "/plugins");
+  return strcat(getenv("PATH"), "/plugins");
+}
+
+struct Plugin *loadPlugins() {
+  const char *pluginDir = getPluginDir();
+  struct Plugin *p = malloc(sizeof(struct Plugin));
+  
+  DIR *openDIR = opendir(pluginDir);
+  if (openDIR == NULL) { printErr("opendir failed"); }
+
+  struct dirent *curr_plugin;
+  int count = 0;
+  do {
+    curr_plugin = readdir(openDIR);
+    // Process each plugin
+    char *name = curr_plugin->d_name;
+    int len = strlen(name);
+    // Check for proper extension
+    if (len > 2 && strcmp((name+len-3), ".so") == 0) {
+      p = realloc(p, ++count * sizeof(struct Plugin));
+
+      // Store handle
+      char *temp = malloc(256);
+      strcpy(temp, pluginDir);
+      strcat(temp, name);
+      void *h = dlopen(temp, RTLD_LAZY);
+      if (!h) { printErr("bad plugin handle"); }
+      (p+count)->handle = h;
+
+      // Store function addresses
+      loadFuncs(h, p+count);
+    }
+  } while (curr_plugin != NULL);
+
+  if (closedir(openDIR)) { printErr("closedir failed"); }
+  return p;
+}
+
+void loadFuncs(void *handle, struct Plugin *p) {
+  *(void**) (&(p->get_plugin_name)) = dlsym(handle, "get_plugin_name");
+  *(void**) (&(p->get_plugin_desc)) = dlsym(handle, "get_plugin_desc");
+  *(void**) (&(p->parse_arguments)) = dlsym(handle, "parse_arguments");
+  *(void**) (&(p->transform_image)) = dlsym(handle, "transform_image");
+
+  // Check for failure
+  if (!p->get_plugin_name || !p->get_plugin_desc ||
+      !p->parse_arguments || !p->transform_image) {
+    printErr("failure storing function address");
+  }
+}
+
+void printPlugins(struct Plugin *p, int num) {
+  printf("Loaded %d plugin(s)\n", num);
+  for (int i = 0; i < num; i++) {
+    printf("%8s: %s\n", p->get_plugin_name(), p->get_plugin_desc());
+    p = p + 1;
+  }
 }
--- csf_assign04/imgproc.h
+++ csf_assign04/imgproc.h
@@ -4,8 +4,19 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <dirent.h>
+#include <dlfcn.h>
 #include "image.h"
 #include "pnglite.h"
+
+/* Plugin struct for plugin handling */
+struct Plugin {
+  void *handle;
+  const char *(*get_plugin_name)(void);
+  const char *(*get_plugin_desc)(void);
+  void *(*parse_arguments)(int num_args, char *args[]);
+  struct Image *(*transform_image)(struct Image *source, void *arg_data);
+};
 
 /*
  * Method to print usage message on incorrect
@@ -27,11 +38,22 @@ void printErr(const char *msg);
  */
 char *getPluginDir();
 
-/* Plugin struct for plugin handling */
-struct Plugin {
-  void *handle;
-  const char *(*get_plugin_name)(void);
-  const char *(*get_plugin_desc)(void);
-  void *(*parse_arguments)(int num_args, char *args[]);
-  struct Image *(*transform_image)(struct Image *source, void *arg_data);
-};
+/*
+ * Method to create dynamically allocated
+ * array of Plugins structures with all relevant
+ * information for Plugin manipulation
+ */
+struct Plugin *loadPlugins();
+
+/*
+ * Load functions associated with a
+ * plugin handle.
+ */
+void loadFuncs(void *handle, struct Plugin *p);
+
+/*
+ * Function to print list of plugins
+ * given the plugin-storing data structure
+ * and the total number of plugins.
+ */
+void printPlugins(struct Plugin *p, int num);
